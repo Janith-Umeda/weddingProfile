8a7412a77fd86fa55ec620a569ce3c4ed2e97c24
Janith-Umeda <strakzzm@gmail.com>
2023-07-25 23:04:06 +0530

    new changes for RSVP Form

--- src/components/rsvpForm.jsx
+++ src/components/rsvpForm.jsx
@@ -1,4 +1,5 @@
 import {motion,useScroll} from 'framer-motion';
+import { Button, Card, Form } from 'react-bootstrap';
 
 export default function RSVPForm(){
 
@@ -18,6 +19,30 @@ export default function RSVPForm(){
                     // style={{translateY:scrollY}}
                     loading='lazy'
                 />
+                <div className="form-container">
+                    <Card>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" />
+                                <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                                </Form.Text>
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                                <Form.Check type="checkbox" label="Check me out" />
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                        </Form>
+                    </Card>
+                </div>
             </div>
         </div>
     );
